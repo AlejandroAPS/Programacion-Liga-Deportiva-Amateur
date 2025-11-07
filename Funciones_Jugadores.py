ba6567066de77a6f6976d56efeb62a4988a663d2
AlejandroAPS <alejandroagustin.perezserrano.25@campusfp.es>
2025-11-07 11:35:22 +0100

    PARTE 2 COMPLETADA CREA UN BRANCH Y DEJA DE USAR EL MAIN BOLUDIN
    Parte 2 terminada,en principio hemos testeado todas las funiones y a simple vista fuciona todo como debería

--- Funciones_Jugadores.py
+++ Funciones_Jugadores.py
@@ -10,13 +10,13 @@ def menu_jugadores_interno():
         opcion = int(input("Escoge el número de la opción que desees: "))
         match opcion:
             case 1:
-                Funciones_Jugadores.alta_jugador1(LigaDeportivaAmateur.jugadores)
+                Funciones_Jugadores.alta_jugador1(LigaDeportivaAmateur.jugadores, LigaDeportivaAmateur.equipos)
             case 2:
                 Funciones_Jugadores.ver_jugador2(LigaDeportivaAmateur.jugadores)
             case 3:
                 Funciones_Jugadores.buscar_ID_jugador3(LigaDeportivaAmateur.jugadores)
             case 4:
-                Funciones_Jugadores.actualizar_jugador4(LigaDeportivaAmateur.jugadores)
+                Funciones_Jugadores.actualizar_jugador4(LigaDeportivaAmateur.jugadores, LigaDeportivaAmateur.equipos)
             case 5:
                 Funciones_Jugadores.eliminar_jugador5(LigaDeportivaAmateur.jugadores)
             case 6:
@@ -28,44 +28,59 @@ def menu_jugadores_interno():
                 
 def alta_jugador1(jugadores, equipos):
     nuevojugador = {}
-    nuevonombre = str(input("Dime el nombre del equipo:"))
+    nuevonombre = str(input("Dime el nombre del jugador:"))
     nuevojugador.update({"ID": len(jugadores) + 1, "nombre": nuevonombre})
     nuevoposicion = str(input("Dime su posición como jugador:")).strip().lower()
-    nuevojugador.update({"precio": nuevoposicion })
-    booleano = input("Se encuentran actvivos en la liga? (si/no):").strip().lower()    #Esta parafernalia es porque al hacer bool(input()) si esta lleno da true y si esta vacio da false por lo que siempre daba false porque python es mierda
+    nuevojugador.update({"posicion": nuevoposicion })
+    booleano = input("Se encuentra activo en la liga? (si/no):").strip().lower()    #Esta parafernalia es porque al hacer bool(input()) si esta lleno da true y si esta vacio da false por lo que siempre daba false porque python es mierda
     nuevoactividad = booleano in ["si", "sì", "s", "true", "1"]   #(Me ayudo chatGPT) es para comparar el input de usuaior a las opciones de esta lista y  asi de true o flase bien porque antes solo devolvia true
     nuevojugador.update({"activo": nuevoactividad })
     nuevoequipo = str(input("En que equipo juega el jugador?"))
     for equipo in equipos:
         if nuevoequipo == equipo["nombre"]:
-            nuevojugador.update({"id_equipo":nuevoequipo})
-            print("Se ha linkeado el jugador al equipo")
+            nuevojugador.update({"nombre_equipo":nuevoequipo})
+            print("Equipo asociado corectamente")
     jugadores.append(nuevojugador) 
     return jugadores
 
 
 def ver_jugador2(jugadores): 
-    print(tabulate(jugadores))
-
+    print("------------------")
+    print("1.De toda la liga")
+    print("2.De un equipo")
+    print("------------------")
+    eleccion = int(input("Quieres listar todos los jugadores de la liga o los de un equipo en particular?"))
+    if eleccion == 1:
+        print(tabulate(jugadores))  
+    elif eleccion == 2:
+        jugadoresxequipo = []   #Se crea una nueva lista para guardar todo los jugadores del equipo seleccionado
+        cualequipo = str(input("De que equipo quieres buscar sus jugadores?"))
+        for jugador in jugadores:
+            if jugador["nombre_equipo"] == cualequipo:
+                jugadoresxequipo.append(jugador)
+        print(tabulate(jugadoresxequipo))
+    else:
+        print("Equipo no encontrado")
+        
 def buscar_ID_jugador3(jugadores):
-    busqueda = int(input("Que ID de equipo quieres buscar?"))
+    busqueda = int(input("Que ID de jugador quieres buscar?"))
     if busqueda < len(jugadores) + 1:
         for jugador in jugadores:
             if jugador["ID"] == busqueda:
                 print(jugador)
     else:
-        print("Artículo no encontrado")
+        print("Jugador no encontrado")
         
 def actualizar_jugador4(jugadores, equipos):
     jugador_encontrado = False    #Esto es importante luego
-    nombre_jugador = str(input("¿Cual es el nombre del equipo a actualizar?: "))
+    nombre_jugador = str(input("¿Cual es el nombre del jugador a actualizar?: "))
     for jugador in jugadores:
         if  nombre_jugador == jugador["nombre"]:
             jugador_encontrado = True
             jugador_actualizado = jugador
-            print("Equipo encontrado: ", jugador)
+            print("Jugador encontrado: ", jugador)
             print("Campos disponibles para actualizar:")
-            for clave in jugadores:
+            for clave in jugador:
                 if clave != "ID":
                     print(clave)
             clave = str(input("Que campo deseas actualizar?")).strip().lower()
@@ -93,21 +108,21 @@ def actualizar_jugador4(jugadores, equipos):
                 case _:
                     print("Opción de campo erronea")
     if jugador_encontrado == False:    #Evita posibles comportamientos extraños/bugs etc (Si lo tocas te toco)
-        print("Equipo no encontrado")
+        print("Jugador no encontrado")
     return jugador_actualizado, jugadores
 
 def eliminar_jugador5(jugadores):
     jugador_encontrado = False    
-    nombre_equipo = str(input("¿Cual es el nombre del equipo a eliminar?: "))
+    nombre_equipo = str(input("¿Cual es el nombre del jugador a eliminar?: "))
     for jugador in jugadores:
         if jugador["nombre"] == nombre_equipo:
             jugador_encontrado = True
-            print("Artículo encontrado: ", jugador)
-            pythonesmierda = (input("Estas seguro de que quieres borrar el artículo(Acción irreversible)(si/no):")).strip().lower()
+            print("Jugador encontrado: ", jugador)
+            pythonesmierda = (input("Estas seguro de que quieres borrar el jugador?(Acción irreversible)(si/no):")).strip().lower()
             seguro = pythonesmierda in ["si", "sì", "s", "true", "1"]  #Es para que le puedas meter el bicho HAY SI DIQUE YO QUIERO QUE ME METAN EL BICHO(lo explico arriba)
             if seguro == True:
                jugadores.remove(jugador) 
-               print("Artículo eliminado correctamente")
+               print("Jugador eliminado correctamente")
 
     if jugador_encontrado == False:
         print("Artículo no encontrado")
